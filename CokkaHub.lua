62a351ab1d25487ef526ed07fc983cef7b79855d
Privacy <109567156+UserDevEthical@users.noreply.github.com>
2024-04-27 00:19:26 +0900

    Update CokkaHub.lua

--- CokkaHub.lua
+++ CokkaHub.lua
@@ -1,6 +1,7 @@
 repeat task.wait() until game:IsLoaded()
 local TablePlace = {7449423635,2753915549,4442272183}
-if table.find(TablePlace,game.PlaceId) then 
+if table.find(TablePlace,game.PlaceId) then
+    _G.Game = "BF"
     loadstring(game:HttpGet("https://raw.githubusercontent.com/UserDevEthical/Loadstring/main/BF-New.lua"))()
 else
     game.Players.LocalPlayer:Kick("not Support")
