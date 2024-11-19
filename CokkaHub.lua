2015e29891a34724bfd90f671f21909b771a06cf
Privacy <109567156+UserDevEthical@users.noreply.github.com>
2024-11-19 18:48:32 +0900

    Update CokkaHub.lua

--- CokkaHub.lua
+++ CokkaHub.lua
@@ -1,5 +1,11 @@
 repeat task.wait() until game:IsLoaded()
 local TablePlace = {7449423635,2753915549,4442272183}
+game:GetService("StarterGui"):SetCore("SendNotification",{
+	Title = "Warning!", -- Required
+	Text = "I don't sell Cokka Hub, be careful when buying from someone or you will lose money:)", -- Required
+	Icon = "rbxassetid://9709149431", -- Optional
+    Duration = 20
+})
 if table.find(TablePlace,game.PlaceId) then
     _G.Game = "BF"
     loadstring(game:HttpGet("https://raw.githubusercontent.com/UserDevEthical/Loadstring/main/BF-New.lua"))()
