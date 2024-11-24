fa43948728d6405ec21ffa9e374d9f1a72d5dfef
Privacy <109567156+UserDevEthical@users.noreply.github.com>
2024-11-25 00:34:59 +0900

    Update CokkaHub.lua

--- CokkaHub.lua
+++ CokkaHub.lua
@@ -2,7 +2,7 @@ repeat task.wait() until game:IsLoaded()
 local TablePlace = {7449423635,2753915549,4442272183}
 game:GetService("StarterGui"):SetCore("SendNotification",{
 	Title = "Read", -- Required
-	Text = "Use Arceus or Codex Executor (Delta is crashing with my script)", -- Required
+	Text = "If you use Delta then use other executor (Delta is crashing with my script)", -- Required
 	Icon = "rbxassetid://9709149431", -- Optional
 	Duration = 5
 })
