6e46046740486642fcf9fac1a867914a2ca75f6c
Privacy <109567156+UserDevEthical@users.noreply.github.com>
2024-12-07 02:05:18 +0900

    Update CokkaHub.lua

--- CokkaHub.lua
+++ CokkaHub.lua
@@ -2,7 +2,7 @@ repeat task.wait() until game:IsLoaded()
 local TablePlace = {7449423635,2753915549,4442272183}
 game:GetService("StarterGui"):SetCore("SendNotification",{
 	Title = "Read", -- Required
-	Text = "Working", -- Required
+	Text = "i don't sell it", -- Required
 	Icon = "rbxassetid://9709149431", -- Optional
 	Duration = 5
 })
