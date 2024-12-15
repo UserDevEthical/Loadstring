b0543760eef74c4345fce7f4a0cbaf8712ed25f3
Privacy <109567156+UserDevEthical@users.noreply.github.com>
2024-12-15 22:37:28 +0900

    Update CokkaHub.lua

--- CokkaHub.lua
+++ CokkaHub.lua
@@ -2,9 +2,9 @@ repeat task.wait() until game:IsLoaded()
 local TablePlace = {7449423635,2753915549,4442272183}
 game:GetService("StarterGui"):SetCore("SendNotification",{
 	Title = "Read", -- Required
-	Text = "I'm updating, Wait me ~", -- Required
+	Text = "I'm updating, Wait me ~~~", -- Required
 	Icon = "rbxassetid://9709149431", -- Optional
-	Duration = 5
+	Duration = 999
 })
 if table.find(TablePlace,game.PlaceId) then
     _G.Game = "BF"
