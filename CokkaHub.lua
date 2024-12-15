5d528eec1c6e4d8f34d0ffd4525a1df80e839781
Privacy <109567156+UserDevEthical@users.noreply.github.com>
2024-12-15 22:38:30 +0900

    Update CokkaHub.lua

--- CokkaHub.lua
+++ CokkaHub.lua
@@ -2,7 +2,7 @@ repeat task.wait() until game:IsLoaded()
 local TablePlace = {7449423635,2753915549,4442272183}
 game:GetService("StarterGui"):SetCore("SendNotification",{
 	Title = "Read", -- Required
-	Text = "I'm updating, Wait me ~~~", -- Required
+	Text = "I'm Updating, Wait me ~~~", -- Required
 	Icon = "rbxassetid://9709149431", -- Optional
 	Duration = 999
 })
