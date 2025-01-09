9af1578d24dc1a6e44cbe6645bdb5b27bcb108aa
Privacy <109567156+UserDevEthical@users.noreply.github.com>
2025-01-09 15:34:50 +0900

    Update CokkaHub.lua

--- CokkaHub.lua
+++ CokkaHub.lua
@@ -2,7 +2,7 @@ repeat task.wait() until game:IsLoaded()
 local TablePlace = {7449423635,2753915549,4442272183}
 game:GetService("StarterGui"):SetCore("SendNotification",{
 	Title = "Read", -- Required
-	Text = "Temporary lag, Stable update will be out next day, Thanks for the support!!", -- Required
+	Text = "We are still working on the update, thanks for the support!", -- Required
 	Icon = "rbxassetid://9709149431", -- Optional
 	Duration = 10
 })
