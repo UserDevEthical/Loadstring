3c0eee2af428930dd295e418385e0167a90b1a93
Privacy <109567156+UserDevEthical@users.noreply.github.com>
2025-01-16 16:18:52 +0900

    Update CokkaHub.lua

--- CokkaHub.lua
+++ CokkaHub.lua
@@ -2,7 +2,7 @@ repeat task.wait() until game:IsLoaded()
 local TablePlace = {7449423635,2753915549,4442272183}
 game:GetService("StarterGui"):SetCore("SendNotification",{
 	Title = "Read", -- Required
-	Text = "If the Script doesn't work, Let's try another Executors!", -- Required
+	Text = "Wait...", -- Required
 	Icon = "rbxassetid://9709149431", -- Optional
 	Duration = 15
 })
