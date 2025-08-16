8e62ea0e9b82ee31e27ec215adec9c43e9380352
Privacy <109567156+UserDevEthical@users.noreply.github.com>
2025-08-16 23:23:28 +0700

    Update CokkaHub.lua

--- CokkaHub.lua
+++ CokkaHub.lua
@@ -2,7 +2,7 @@ repeat task.wait() until game:IsLoaded()
 local TablePlace = {7449423635,2753915549,4442272183, 126884695634066}
 game:GetService("StarterGui"):SetCore("SendNotification",{
 	Title = "Read!", -- Required
-	Text = "Loading... - Fixing Fast Attack... ", -- Required
+	Text = "Loading... - Gag scip is out! ", -- Required
 	Icon = "rbxassetid://9709149431", -- Optional
 	Duration = 15
 })
@@ -13,6 +13,7 @@ if table.find(TablePlace,game.PlaceId) then
 else
     game.Players.LocalPlayer:Kick("not Support")
 end
+
 
 
 
