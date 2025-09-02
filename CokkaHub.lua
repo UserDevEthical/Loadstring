4e4a0e281d4e89006cfccc5582415d3f655c666c
Privacy <109567156+UserDevEthical@users.noreply.github.com>
2025-09-02 14:10:14 +0700

    Update CokkaHub.lua

--- CokkaHub.lua
+++ CokkaHub.lua
@@ -42,7 +42,7 @@ function Loader.new()
             pcall(function()
                 game:GetService("StarterGui"):SetCore("SendNotification", {
                     Title = "Loader",
-                    Text = "Loading " .. name .. "\n(Ty for using!)",
+                    Text = "Loading " .. name .. "\n(Maintenance...)",
                     Icon = "rbxassetid://9709149431",
                     Duration = 15
                 })
@@ -62,3 +62,4 @@ Load:add("Blox Fruits", {7449423635, 2753915549, 4442272183}, {"https://raw.gith
 Load:add("Grow A Garden", {126884695634066}, {"https://raw.githubusercontent.com/UserDevEthical/Loadstring/main/Loader.lua"})
 
 Load:run(true) -- true & false
+
